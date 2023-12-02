f57af7d010221baf227179600f1d1f4332590613
Orkhan11980 <115529584+Orkhan11980@users.noreply.github.com>
2023-12-02 15:30:46 +0400

    data display

--- productDetail.js
+++ productDetail.js
@@ -1,9 +1,8 @@
 $(document).ready(function() {
-    
     var productId = new URLSearchParams(window.location.search).get('productId');
     if (productId) {
         fetchProductDetails(productId);
-    }else {
+    } else {
         console.error("No product ID found in URL.");
     }
 });
@@ -16,38 +15,56 @@ function fetchProductDetails(productId) {
             displayProductDetails(product);
         },
         error: function(xhr, status, error) {
-            console.error("Error occured fetching product details:", error);
-           
+            console.error("Error occurred fetching product details:", error);
         }
     });
 }
 
 function displayProductDetails(product) {
-    var container = $('#product-info');
-    container.empty(); // Make sure to clear previous content
+    var container = $('#detail-container'); 
+    container.empty();
 
     var detailHtml = `
-        <div class="product-card">
-            <div class="product-image">
-                <img src="${product.images[0]}" alt="${product.title}">
-            </div>
-            <div class="product-info">
-                <h2 class="product-title">${product.title}</h2>
-                <p class="product-price">Price: ${product.price}</p>
-                <p class="product-discount">Discount: ${product.discountPercentage}%</p>
-                <p class="product-category">Category: ${product.category}</p>
-            
-                <p class="product-stock">Stock: ${product.stock}</p>
-            
-            </div>
+       
+    <div class="detail-card"> 
+    <div class="content-wrapper">
+    <div class="detail-image-container">
+        <img src="${product.images[0]}" alt="${product.title}" class="main-detail-image">
+        <div class="detail-thumbnails"> 
+            ${product.images.map(image => `<img src="${image}" alt="${product.title}" class="thumbnail-image">`).join('')} 
         </div>
-        <div id="product-images" class="product-images-container">
-            ${product.images.map(image => `<img src="${image}" alt="${product.title}" class="product-detail-image">`).join('')}
-        </div>
-    `;
+    </div>
+    <div class="detail-info"> 
+        <h2 class="detail-title">${product.title}</h2> 
+        <p class="detail-price">Price: $${product.price}</p> 
+        <p class="detail-desc">Description: ${product.description}</p>
+        <p class="detail-rating">Rating: ${product.rating}</p>
+        <p class="detail-discount">Discount: ${product.discountPercentage}%</p> 
+        <p class="detail-category">Category: ${product.category}</p> 
+        <p class="detail-stock">Stock: ${product.stock}</p> 
+    </div>
+    </div>
+    <div class="go-back-container">
+        <button id="goBackButton">Go Back</button>
+    </div>
+</div> 
+
+
+      
+    `
     container.append(detailHtml);
+
+    $('#goBackButton').on('click', function() {
+        
+        goBackToMainView(); 
+    });
 }
-$(document).on('click', '.product-detail-image', function() {
+
+$(document).on('click', '.detail-image-item', function() { 
     var src = $(this).attr('src');
     console.log('Image clicked:', src);
 });
+
+function goBackToMainView() {
+    window.history.back();
+}
